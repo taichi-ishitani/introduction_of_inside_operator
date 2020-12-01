0e1898f5b5fce7be57aea4afbf0b1c3838273d13
Taichi Ishitani <taichi730@gmail.com>
2020-12-01 21:41:09 +0900

    範囲での $ の使用例を追加

--- sample_2.sv
+++ sample_2.sv
@@ -2,9 +2,12 @@ module sample_2 (
   input   var [1:0] i_d,
   input   var [1:0] i_min,
   input   var [1:0] i_max,
-  output  var       o_in_range
+  output  var [3:0] o_in_range
 );
   always_comb begin
-    o_in_range  = i_d inside {[i_min:i_max]};
+    o_in_range[0] = i_d inside {[i_min:i_max]};
+    o_in_range[1] = i_d inside {[    $:i_max]};
+    o_in_range[2] = i_d inside {[i_min:    $]};
+    o_in_range[3] = i_d inside {[    $:    $]};
   end
 endmodule
